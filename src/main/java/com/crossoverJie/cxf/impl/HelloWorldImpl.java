f8f1a329f23b79ade06a1236e5bd1b9595d1ba6a
voidException <noexception@126.com>
2017-10-18 22:06:05 +0800

    Initial commit

---
+++ src/main/java/com/crossoverJie/cxf/impl/HelloWorldImpl.java
@@ -0,0 +1,16 @@
+package com.crossoverJie.cxf.impl;
+
+import com.crossoverJie.cxf.HelloWorld;
+import org.springframework.stereotype.Component;
+
+import javax.jws.WebService;
+
+@Component("helloWorld")
+@WebService
+public class HelloWorldImpl implements HelloWorld {
+
+	public String say(String str) {
+		return "Hello"+str;
+	}
+
+}
